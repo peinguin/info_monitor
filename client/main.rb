2a83b94ea81c9f8ea5dd8d969b488292cb84dece
peinguin <pingvein@gmail.com>
2012-12-03 22:41:50 +0200

    reload if error

--- client/main.rb
+++ client/main.rb
@@ -2,10 +2,47 @@
 require 'Qt4'
 require 'qtwebkit'
 
-Qt::Application.new(ARGV) do
-	Qt::WebView.new do
-		self.load Qt::Url.new(ARGV[0])
-		show
+class WebPage < Qt::WebPage
+	def initialize(wnd=nil)
+		super
+		@wnd = wnd
 	end
-	exec
+    def javaScriptConsoleMessage(msg, lineNumber, sourceID)
+    	super msg, lineNumber, sourceID
+    	puts(sourceID, lineNumber, msg)
+    	@wnd.reload
+    end
 end
+class Window < Qt::Widget
+	def initialize(url,parent=nil)
+		super parent
+
+		@url = url
+
+		@view = Qt::WebView.new
+    	@view.setPage(WebPage.new(self))
+
+		reload
+
+	    layout = Qt::VBoxLayout.new(self)
+	    layout.setMargin(0)
+	    layout.addWidget(@view)
+	end
+
+	def view
+      @view
+    end
+    def view=(val)
+      @view = val
+    end
+
+    def reload
+    	@view.load Qt::Url.new(@url)
+    end
+end
+
+app = Qt::Application.new(ARGV)
+Window.new(ARGV[0]) do
+	show
+end
+app.exec
